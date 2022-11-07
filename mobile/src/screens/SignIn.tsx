bb4d668f77a6b89e48763dfccebd2c2da1fae044
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-11-07 08:53:00 -0300

    Mobile [ Update ]
    [ Mobile ]
    • Interface de Autenticação:
      - Utilizando SVG como Componente no React Native para exibir o Logo (svg transformer)
      - Tipando o SVG
      - Criando o componente de Button
      - Criando o componente de SignIn

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,9 +1,30 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from '@expo/vector-icons'
 
-export function SignIn(){
-    return (
-        <Center flex={1} bgColor="gray.900" >
-        <Text color="white" fontSize={24} fontFamily="heading">SignIn</Text>
-      </Center>
-    )
+import Logo from '../assets/logo.svg'
+import { Button } from "../components/Button";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900" p={7} >
+      <Logo width={212} height={40} />
+
+      <Button 
+        type={'SECONDARY'}
+        title="ENTRAR COM O GOOLE"
+        leftIcon={<Icon as={Fontisto} name='google' color='white' size='md' />}
+        isLoading={false}
+        mt={12}
+      />
+
+      <Text
+        color={'white'}
+        textAlign={'center'}
+        mt={4}
+      >
+        Não utilizamos nenhuma informação além {'\n'}
+        do seu e-mail para a criação de sua conta.
+      </Text>
+    </Center>
+  )
 }
